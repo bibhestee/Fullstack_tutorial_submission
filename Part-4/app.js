7b9fa8d33835c3b0301cd04347c5049efd699aed
Bibest <bibhestee@gmail.com>
2023-03-25 21:56:37 +0100

    Middlewares added
    errorHandler, requestLogger and unknownEndpoint added
    The application is working perfectly with the improvement.
    
    errorHandler - Handles the errors related to Cast Error or Validation
    error and forward any other error to the unknownEndpoint.
    
    requestLogger - Log all the request information to the console.
    
    unknownEndpoint - Handles the errors passed by the next function sent
    from the errorHandler and responde with the error - unknownEndpoint

--- Part-4/app.js
+++ Part-4/app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const blogRouter = require('./controllers/blogs')
 const {MONGODB_URI} = require('./utils/config')
 const {info, error} = require('./utils/logger')
+const {requestLogger, unknownEndpoint, errorHandler} = require('./utils/middleware')
 
 const app = express()
 
@@ -20,8 +21,13 @@ mongoose.connect(MONGODB_URI)
 // Middleware
 app.use(cors())
 app.use(express.json())
+app.use(requestLogger)
 
 // Router
 app.use('/api/blogs', blogRouter)
+
+// Middleware - Always add these middlewares at the end
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 module.exports = app
