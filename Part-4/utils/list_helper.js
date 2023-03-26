9763e02d9796115fb9849fb1c46e4aea068422f7
Bibest <bibhestee@gmail.com>
2023-03-26 12:40:23 +0100

    4.4 Completed: All tests passed!

--- Part-4/utils/list_helper.js
+++ Part-4/utils/list_helper.js
@@ -1,7 +1,22 @@
 const dummy = (blogs) => {
-    return 1
+    return 1;
+}
+
+const totalLikes = (blogs) => {
+    const sumAllLikes = (blogs) => {
+        let total = 0;
+        blogs.map((blog) => {
+            total += blog.likes;
+        })
+        return total;
+    }
+
+
+    return blogs.length > 0
+    ?  sumAllLikes(blogs)
+    :  0
 }
 
 module.exports = {
-    dummy
+    dummy, totalLikes
 }
